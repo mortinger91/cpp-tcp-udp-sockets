126bf7e5a00332da082535664a9327c2243b3548
mortinger91 <micheleemiliani91@gmail.com>
2023-10-27 19:45:15 +0200

    Add UDP launch options

--- ProjectFolder/src/main.cpp
+++ ProjectFolder/src/main.cpp
@@ -5,19 +5,33 @@
 
 int main(int argc, char* argv[])
 {
-    if (argc >= 2)
+    if (argc >= 3)
     {
-        if ((strcmp(argv[1], "server") == 0) && argc == 3)
+        Protocol protocol;
+        if (strcmp(argv[2], "tcp") == 0)
         {
-            Server s(Protocol::TCP, std::stoi(argv[2]));
+            protocol = Protocol::TCP;
+        }
+        else if (strcmp(argv[2], "udp") == 0)
+        {
+            protocol = Protocol::UDP;
+        }
+        else
+        {
+            return 5;
+        }
+
+        if ((strcmp(argv[1], "server") == 0) && argc == 4)
+        {
+            Server s(protocol, std::stoi(argv[3]));
             if (!s.start())
             {
                 return 4;
             }
         }
-        else if (strcmp(argv[1], "client") == 0 && argc == 4)
+        else if (strcmp(argv[1], "client") == 0 && argc == 5)
         {
-            Client c(Protocol::TCP, std::string(argv[2]), std::stoi(argv[3]));
+            Client c(protocol, std::string(argv[3]), std::stoi(argv[4]));
             if (!c.start())
             {
                 return 3;
